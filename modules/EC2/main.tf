3fc233590185e462520df9b1f2fe05bf85be033a
nprabhu <nprabhu26@gmail.com>
2025-06-02 10:25:34 -0400

    code structure changed

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -1,16 +1,13 @@
 #EC2 resourece
 resource "aws_instance" "npd_Instance01" {
 
-    ami = "ami-084568db4383264d4"
-    instance_type = "t2.micro"
-    availability_zone = ""
+  ami               = "ami-084568db4383264d4"
+  instance_type     = "t2.micro"
+  availability_zone = ""
+  security_groups   = ""
+  tags = {
+    Name = "Instance 01"
+  }
 
-    security_groups = ""
 
-
-    tags = {
-      Name = "Instance 01"
-    }
-
-  
 }
