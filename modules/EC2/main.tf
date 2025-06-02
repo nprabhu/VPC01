9b79939477044ad853c153f60df3decc2736bc2f
nprabhu <nprabhu26@gmail.com>
2025-06-02 10:21:23 -0400

    Security group added

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "npd_Instance01" {
     ami = "ami-084568db4383264d4"
     instance_type = "t2.micro"
 
+    security_groups = ""
+
 
     tags = {
       Name = "Instance 01"
