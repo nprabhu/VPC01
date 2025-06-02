ba630ca7036ecad1383ae1fb40a82febe3f665fb
nprabhu <nprabhu26@gmail.com>
2025-06-02 10:34:02 -0400

    locals block added

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -1,3 +1,11 @@
+
+
+locals {
+  subnets_all = concat(var.subnet_ids_public, var.subnet_ids_private)
+  azs         = data.aws_availability_zones.available.names
+}
+
+
 #EC2 resourece
 resource "aws_instance" "npd_Instance01" {
 
